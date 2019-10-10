89722f02b3f050f39444d80a668da78a98eda0fb
Vinicius <vncastanheira@gmail.com>
2019-10-10 15:40:54 -0300

    menu animation

---
+++ index.js
@@ -0,0 +1,12 @@
+var opened = true; 
+function toggleMenu() {
+    var menu = document.getElementById("menu")
+    if (opened) {
+        menu.style.width = "0%"
+        opened = false
+    }
+    else {
+        menu.style.width = "75%"
+        opened = true
+    }
+}
