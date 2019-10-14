600be39d92451658c5704a53af4d1e8550b260e4
Vinicius <vncastanheira@gmail.com>
2019-10-14 15:06:22 -0300

    close menu on outside click

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-var opened = false; 
-function toggleMenu() {
-    var menu = document.getElementById("menu")
-    if (opened) {
-        menu.style.width = "0%"
-        opened = false
-    }
-    else {
-        menu.style.width = "75%"
-        opened = true
-    }
+const menu = document.getElementById("menu")
+let opened = false;
+function closeMenu(e) {
+    menu.style.width = "0%"
+    opened = false;
 }
+
+function openMenu(e) {
+    menu.style.width = "75%"
+    opened = true;
+}
+
+$("#btn-menu-small").click(function (event) {
+    openMenu();
+    e.stopPropagation();
+})
+
+$("#btn-menu-large").click(function (event) {
+    openMenu();
+    e.stopPropagation();
+})
+
+document.addEventListener("click", (e) => {
+    if (opened) {
+        let btnLarge = document.getElementById("btn-menu-large") 
+        let btnSmall = document.getElementById("btn-menu-small")
+        let menuClose = document.getElementById("menu-close")
+        if (e.target != menu
+            && e.target != btnLarge
+            && e.target != btnSmall
+            && e.target != menuClose) {
+                closeMenu()
+        }
+    }
+})
